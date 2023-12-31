31c9ee948f6c2bfce5944234db9c495f28dd0b20
Abhishek-IITKGP <ak1836716@kgpian.iitkgp.ac.in>
2023-12-31 09:42:39 +0530

    exception handelled

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ __version__ = '0.0.0'
 
 REPO_NAME = 'text-summarizer'
 AUTHOR_USER_NAME = 'Abhishek-IITKGP'
-SRC_REPO = 'text-summarizer'
+SRC_REPO = 'textSummarizer'
 AUTHOR_EMAIL = 'ak1836716@gmail.com'
 
 setuptools.setup(
